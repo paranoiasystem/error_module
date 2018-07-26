f71a650ada4dc3b01e297584baece71c2db04ceb
Marco Ferraioli <overflowsystem03@gmail.com>
2018-07-26 10:02:32 +0200

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+module.exports.errorHandler = (err, req, res, next) => {
+    res.status(err.status || 500).json({
+        errorCode: err.status,
+        message: err.message,
+        userInput: req.body,
+        stackTrace: ((err.prevError == null || err.prevError == '') ? '' : err.prevError),
+        timestamp: Date.now(),
+        token: req.get('Authorization'),
+        uri: err.uri,
+        method: err.method
+    });
+};
