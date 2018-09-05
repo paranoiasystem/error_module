20b32c9b21ccb3c61f80f128c4e2184bbb12c50f
Marco Ferraioli <m.ferraioli25@studenti.unisa.it>
2018-09-05 10:10:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-var bestemmia_module = require("bestemmia_module");
-
 module.exports.errorHandler = (err, req, res, next) => {
     res.status(err.status || 500).json({
         errorCode: err.status,
@@ -9,7 +7,6 @@ module.exports.errorHandler = (err, req, res, next) => {
         timestamp: Date.now(),
         token: req.get('Authorization'),
         uri: err.uri,
-        method: err.method,
-        bestemmia_del_giorno: bestemmia_module.getBestemmia()
+        method: err.method
     });
 };
